6b26a6883c1a31e97d9c31c2b47f77f522dfceb4
Ondřej Gajdušek <ogajduse@redhat.com>
2023-12-08 17:33:47 +0100

    add simple dynaconf tester program

---
+++ dynaconf_hooks.py
@@ -0,0 +1,4 @@
+def post(settings):
+    data = {"dynaconf_merge": True}
+    data["NUMBER"] = 789
+    return data
---
+++ program.py
@@ -0,0 +1,7 @@
+from config import settings
+
+assert settings.key == "value"
+assert settings.number == 789
+assert settings.a_dict.nested.other_level == "nested value"
+assert settings['a_boolean'] is False
+assert settings.get("DONTEXIST", default=1) == 1
